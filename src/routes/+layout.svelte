cfd9027ac8785751a9a964299e3c3eebeac81097
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-05-21 18:49:52 +0530

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+    import '../app.css';
+    import {onMount} from 'svelte';
+    import 'aos/dist/aos.css';
+    import AOS from 'aos';
+    
+    import Navbar from '$lib/components/Navbar.svelte';
+
+    onMount(() => {
+         AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+    })
+</script>
+
+<slot />
