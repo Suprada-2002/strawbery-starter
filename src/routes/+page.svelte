7952581de1412c37c51be0d5b290332bc087cb4e
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-05-21 18:48:01 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Hero from "$lib/components/Hero.svelte";
+    import Experiences from "$lib/components/Experiences.svelte";
+</script>
+
+<div>
+<Hero />
+<Experiences />
+</div>
