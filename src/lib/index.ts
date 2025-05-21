a9c518f6bafbeefa2dc1d1f3537cfdc57b824953
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-05-21 18:45:58 +0530

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,37 @@
 // Reexport your entry components here
+export const navLinks = [
+    {label: "Services", href:"#services"},
+    {label: "About", href: "#about"},
+    {label: "join", href: "join"}
+];
+
+export const testimonialData = [
+  {
+    name: "Dilshad Prashad",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    num: "3.9",
+    img: "https://picsum.photos/101/101",
+    occupation:"CEO"
+  },
+  {
+    name: "Prabhakar Mishra",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    num: "4.6",
+    img: "https://picsum.photos/102/102",
+    occupation:"Seniour Manager"
+  },
+  {
+    name: "Johny Pallay",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    num: "5.0",
+    img: "https://picsum.photos/104/104",
+    occupation: "COO"
+  },
+  {
+    name: "Amber Jane",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    num: "4.8",
+    img: "https://picsum.photos/103/103",
+    occupation:"Manager"
+  },
+];
