43972954de833078f69d44a60d4090be9b92d0a1
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-05-22 18:19:16 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
     import Hero from "$lib/components/Hero.svelte";
     import Experiences from "$lib/components/Experiences.svelte";
+    import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <div>
 <Hero />
 <Experiences />
+<Footer />
 </div>
